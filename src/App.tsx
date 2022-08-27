ab8ec4582825aea7324d06cd6815137eae17c926
lukinoo <luka.bliadze.2015@mail.ru>
2022-08-27 15:28:19 +0400

    core(initial-react-app)

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Home } from "./view/home";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>hello, world</h1>
+      <Home />
+    </div>
+  );
+};
+
+export default App;
